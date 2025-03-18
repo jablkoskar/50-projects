38d05539e30ac02acb87280524b7535f02e9b784
Oskar Jabłko <oskar.jablko@hoyergroup.onmicrosoft.com>
2025-03-18 11:05:43 +0100

    Added 10_Dad_jokes

---
+++ 10_Dad_jokes/script.js
@@ -0,0 +1,39 @@
+const joke = document.getElementById("joke");
+const jokeBtn = document.getElementById("jokeBtn");
+
+jokeBtn.addEventListener('click', generateJoke)
+
+generateJoke();
+
+//USING ASYNC/AWAIT
+async function generateJoke() {
+    const config = {
+      headers: {
+        Accept: "application/json",
+      },
+    };
+  
+    const res = await fetch("https://icanhazdadjoke.com/", config)
+    
+    const data = await res.json()
+
+    jokeEl.innerHTML = data.joke;
+    responseId.innerHTML = 'response ID: ' + data.id;
+
+  }
+
+//USING .then()
+    // function generateJoke() {
+    //   const config = {
+    //     headers: {
+    //       Accept: "application/json",
+    //     },
+    //   };
+
+    //   fetch("https://icanhazdadjoke.com/", config)
+    //   .then(res => res.json())
+    //   .then(data => {
+    //     jokeEl.innerHTML = data.joke;
+    //     responseId.innerHTML = 'response ID: ' + data.id;
+    //   });
+    // }
